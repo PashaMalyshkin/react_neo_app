27404dc848f6874306d4cd43a3336e97368a18c5
Pasha <pashamalyshkin2003@gmail.com>
2023-07-13 20:57:30 +0300

    added end-date param

--- src/api/Neo.ts
+++ src/api/Neo.ts
@@ -5,5 +5,5 @@ import {NeoResponse} from "../types/NeoResponse.ts";
 const api_key = import.meta.env.VITE_API_KEY;
 
 export const getNeo = (date: string) => {
-  return client.get<NeoResponse>(`/neo/rest/v1/feed?start_date=${date}&api_key=${api_key}`);
+  return client.get<NeoResponse>(`/neo/rest/v1/feed?start_date=${date}&end_date=${date}&api_key=${api_key}`);
 };
